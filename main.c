a627482a2c43aa85b03d9dce725bca2332a049b9
Valery Ivanov <ivalery111@gmail.com>
2021-08-08 21:31:24 +0300

    feat(main.c): define instruction set

--- main.c
+++ main.c
@@ -20,3 +20,24 @@ enum {
 uint16_t registers[REGS_COUNT];
 /***********************/
 
+/* ==== INSTRUCTION SET ==== */
+enum {
+    OP_BR = 0, /* Branch */
+    OP_ADD,    /* Add */
+    OP_LD,     /* Load */
+    OP_ST,     /* Store */
+    OP_JSR,    /* Jump register */
+    OP_AND,    /* Bitwise `and` */
+    OP_LDR,    /* Load register */
+    OP_STR,    /* Store register */
+    OP_RTI,    /* Unused */
+    OP_NOT,    /* Bitwise `not` */
+    OP_LDI,    /* Load indirect */
+    OP_STI,    /* Store indirect */
+    OP_JMP,    /* Jump */
+    OP_RES,    /* Reserved (unused) */
+    OP_LEA,    /* Load effective address */
+    OP_TRAP,   /* Execute trap */
+};
+/*****************************/
+
