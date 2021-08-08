6fcea7c8c8df6eae7043f53ad81619c7e6308613
Valery Ivanov <ivalery111@gmail.com>
2021-08-08 21:30:45 +0300

    feat(main.c): define registers

--- main.c
+++ main.c
@@ -4,3 +4,19 @@
 static uint16_t memory[UINT16_MAX];
 /********************/
 
+/* ==== REGISTERS ==== */
+enum {
+    R1 = 0,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    IP,	  /* Instruction Pointer */
+    COND, /* Condition Flags */
+    REGS_COUNT,
+};
+uint16_t registers[REGS_COUNT];
+/***********************/
+
